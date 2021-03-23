cc5cdfebba0ab955095d9e5e14f20ca3410668f4
cmd08010 <colleendunion@gmail.com>
2021-03-23 13:02:00 -0400

    Update sign-up route to include the username and name

--- curl-scripts/auth/sign-up.sh
+++ curl-scripts/auth/sign-up.sh
@@ -9,6 +9,8 @@ curl "${API}${URL_PATH}" \
   --header "Content-Type: application/json" \
   --data '{
     "credentials": {
+      "name": "'"${NAME}"'",
+      "username": "'"${USERNAME}"'",
       "email": "'"${EMAIL}"'",
       "password": "'"${PASSWORD}"'",
       "password_confirmation": "'"${PASSWORD}"'"
