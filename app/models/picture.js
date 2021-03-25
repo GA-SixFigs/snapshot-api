960c0fb8f39c9f475fc0c063231fe398807dc204
JamesleeG <jamesleegambardella@gmail.com>
2021-03-25 12:48:20 -0400

    working create with ownership

--- app/models/picture.js
+++ app/models/picture.js
@@ -4,16 +4,16 @@ const pictureSchema = new mongoose.Schema({
   url: {
     type: String,
     required: true
+  },
+  caption: {
+    type: String,
+    required: false
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
   }
-  // caption: {
-  //   type: String,
-  //   required: true
-  // }
-  // owner: {
-  //   type: mongoose.Schema.Types.ObjectId,
-  //   ref: 'User',
-  //   required: true
-  // }
 }, {
   timestamps: true
 })
